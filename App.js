02f894b1547f4b99cca4a10996930a3c0ceebc0d
DmZheleznyak <11lione1991@gmail.com>
2022-04-13 17:11:59 +0300

    safeArea , 1st style

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{padding: 16, backgroundColor: "green"}}>
+          <Text>search</Text>
+        </View>
+        <View style={{flex: 1, padding: 16, backgroundColor: "blue"}}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
+  //   <View style={styles.container}>
+  //     <Text>Open up App.js to start working on your app!</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
